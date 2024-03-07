eaba2120aa60fa5840087a5727e40d9499c7a25c
Diego <almagueralejandro66@gmail.com>
2024-03-07 02:53:17 -0500

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import{saludar}from './js/componentes'
 import './style.css';
-saludar('Juan');
+import { Todo } from './js/classes/todo.class';
+import { TodoList } from './js/classes/todo-list.class';
+
+const tarea=new  Todo('Aprender javascript');
+
+const todoList=new TodoList();
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
